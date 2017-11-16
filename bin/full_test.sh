9a95808c785f4a541787ab045713b7517c50071c
Miguel Vazquez <miguel.vazquez@ntnu.no>
2017-11-16 15:18:13 +0100

    nil

--- bin/full_test.sh
+++ bin/full_test.sh
@@ -6,23 +6,25 @@ for paradigm_in_degree in 5 7 ; do
     for use_steady_state in true false ; do
       for use_observed_synergies in true false ; do
         for obs_method in avg_bliss GS; do
-          for cell_line in AGS SW620 Colo205 SF295; do
-            cmd="drbbt task SINTEF ROC --load_inputs /home/miguelvg/git/workflows/SINTEF/examples/ROC/AGS.slow --ceres_threshold $ceres_threshold --use_steady_state $use_steady_state --use_observed_synergies $use_observed_synergies --obs_method $obs_method --cell_line $cell_line --jn $cell_line -pf --log 1"
-            echo -n "RUN: " >>  log
-            echo $cmd >> log
-            file=`$cmd 2>> log`
-            echo -n "RUN: "
-            echo $cmd
-            echo -n "FILE: "
-            echo $file
-            echo -n "FILE: " >> log
-            echo $file >> log
-            basename=`basename $file`
-            url="$server/SINTEF/ROC/$basename"
-            echo -n "URL: "
-            echo $url
-            echo -n "URL: " >> log
-            echo $url >> log
+          for dir in AGS.slow Topology20171116; do
+            for cell_line in AGS SW620 Colo205 SF295; do
+                cmd="drbbt task SINTEF ROC --load_inputs /home/miguelvg/git/workflows/SINTEF/examples/ROC/$dir --ceres_threshold $ceres_threshold --use_steady_state $use_steady_state --use_observed_synergies $use_observed_synergies --obs_method $obs_method --cell_line $cell_line --jn $cell_line -pf --log 1"
+                echo -n "RUN: " >>  log
+                echo $cmd >> log
+                file=`$cmd 2>> log`
+                echo -n "RUN: "
+                echo $cmd
+                echo -n "FILE: "
+                echo $file
+                echo -n "FILE: " >> log
+                echo $file >> log
+                basename=`basename $file`
+                url="$server/SINTEF/ROC/$basename"
+                echo -n "URL: "
+                echo $url
+                echo -n "URL: " >> log
+                echo $url >> log
+            done
           done
         done
       done
