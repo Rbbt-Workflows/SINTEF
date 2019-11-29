947307b3903c0a8031565bb6448e87b8083436c2
Miguel Vazquez <miguel.vazquez@ntnu.no>
2019-11-29 14:47:10 +0100

    nil

--- lib/rbbt/tasks/observed_synergies.rb
+++ lib/rbbt/tasks/observed_synergies.rb
@@ -48,7 +48,7 @@ module SINTEF
         end
         run = []
         best = 0
-        dose_excess.sort_by{|d,e| d}.each do |d,e|
+        dose_excess.sort_by{|d,e| d.split("-").first.to_f}.each do |d,e|
           v = Misc.mean(e.collect{|_e| _e.to_f})
           v = v.to_f
           if v > 0
@@ -66,6 +66,83 @@ module SINTEF
       next if avg < consecutive_excess_count
       
       syn.first.sub('-','~')
+    end
+  end
+
+  dep :bliss do |jobname, options|
+    case options[:ci_method].to_s
+    when "bliss"
+      {:task => :bliss, :inputs => options, :jobname => jobname}
+    when "hsa"
+    else
+      raise ParameterException, "Synergy method not understood #{options[:ci_method]}"
+    end
+  end
+  dep :hsa do |jobname, options|
+    case options[:ci_method].to_s
+    when "bliss"
+    when "hsa"
+      {:task => :hsa, :inputs => options, :jobname => jobname}
+    else
+      raise ParameterException, "Synergy method not understood #{options[:ci_method]}"
+    end
+  end
+  input :consecutive_excess_count, :integer, "Minimun average synergistic consecutive excess run length (values below threshold in runs not above 0)", 2
+  input :excess_proportional_threshold, :float, "Excess value threshold (negative)", 0.20
+  input :ci_method, :select, "Method to use for calculating synergy value excess", :bliss, :select_options => %w(bliss hsa)
+  task :synergy_classification_by_consecutive_proportional_excesses => :array do |consecutive_excess_count,excess_threshold|
+    parser = TSV::Parser.new dependencies.first
+
+    fields = parser.fields
+    excess_fields = []
+    fields.each_with_index do |field,i|
+      excess_fields << i if field.downcase.include? "excess"
+    end
+
+    dose_fields = []
+    fields.each_with_index do |field,i|
+      dose_fields << i if field.downcase.include? "dose"
+    end
+
+    response_fields = []
+    fields.each_with_index do |field,i|
+      response_fields << i if field.downcase.include? "response"
+    end
+
+
+
+    TSV.traverse parser, :into => :stream do |syn, values|
+      syn = syn.first if Array === syn
+      excess_list = values.values_at *excess_fields
+      dose_list = values.values_at *dose_fields
+      response_list = values.values_at *response_fields
+      num = excess_list.select{|l| l.length > 0}.length
+      counts = dose_list.zip(response_list, excess_list).collect do |dlist, rlist, elist|
+        dose_excess = {}
+        dlist.zip(rlist, elist).each do |d,r,e|
+          dose_excess[d] ||= []
+          additive = r.to_f - e.to_f
+          proportion = r.to_f / additive 
+          dose_excess[d] << proportion
+        end
+        run = []
+        best = 0
+        dose_excess.sort_by{|d,e| d.split("-").first.to_f}.each do |d,e|
+          v = Misc.mean(e.collect{|_e| _e.to_f})
+          v = v.to_f
+          if v < (1 - excess_threshold)
+            run << v
+          end
+          best = run.length if run.length > best
+        end
+        best
+      end
+
+      avg = counts.inject(0){|acc,e| acc += e }.to_f / num
+      
+      next if avg < consecutive_excess_count
+      
+      syn.sub('-','~')
     end
   end
 
@@ -145,8 +222,20 @@ module SINTEF
     selected[cell_line] || []
   end
 
+  dep :synergy_classification_by_consecutive_proportional_excesses do |jobname,options|
+    case options[:syn_method].to_s
+    when "consecutive_excess"
+    when "consecutive_proportional_excess"
+      {:task => :synergy_classification_by_consecutive_proportional_excesses, :inputs => options, :jobname => jobname}
+    when "average_excess"
+    when "GS"
+    else
+      raise ParameterException, "Synergy method not understood #{options[:syn_method]}"
+    end
+  end
   dep :synergy_classification_by_consecutive_excesses do |jobname,options|
     case options[:syn_method].to_s
+    when "consecutive_proportional_excess"
     when "consecutive_excess"
       {:task => :synergy_classification_by_consecutive_excesses, :inputs => options, :jobname => jobname}
     when "average_excess"
@@ -158,6 +247,7 @@ module SINTEF
   dep :synergy_classification_by_avg do |jobname,options|
     case options[:syn_method].to_s
     when "consecutive_excess"
+    when "consecutive_proportional_excess"
     when "average_excess"
       {:task => :synergy_classification_by_avg, :inputs => options, :jobname => jobname}
     when "GS"
@@ -169,13 +259,14 @@ module SINTEF
     case options[:syn_method].to_s
     when "consecutive_excess"
     when "average_excess"
+    when "consecutive_proportional_excess"
     when "GS"
       {:task => :synergy_classification_by_GS, :inputs => options, :jobname => jobname}
     else
       raise ParameterException, "Synergy method not understood #{options[:syn_method]}"
     end
   end
-  input :syn_method, :select, "Source of synergy classifications", :runs, :select_options => %w(consecutive_excess average_excess GS)
+  input :syn_method, :select, "Source of synergy classifications", :runs, :select_options => %w(consecutive_excess consecutive_proportional_excess average_excess GS)
   task :observed_synergies => :array do
     dependencies.first.load
   end
